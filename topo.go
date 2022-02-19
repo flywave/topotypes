ee5773faaf264bda0c310ea3a8a0160a8a53c586
hj <hawking2215@gmail.com>
2022-02-19 18:03:05 +0800

    update

--- topo.go
+++ topo.go
@@ -695,13 +695,13 @@ func TopoMtlToMeshMtl(mtl *TopoMaterial) mst.MeshMaterial {
 }
 
 type TopoTransform struct {
-	Rotation  [4]float64 `json:"rotation"`
-	Translate [3]float64 `json:"translate"`
-	Scale     [3]float64 `json:"scale"`
+	Rotation  *[4]float64 `json:"rotation"`
+	Translate *[3]float64 `json:"translate"`
+	Scale     *[3]float64 `json:"scale"`
 }
 
 func NewTopoTransform() *TopoTransform {
-	return &TopoTransform{Scale: [3]float64{1.0, 1.0, 1.0}}
+	return &TopoTransform{Scale: &[3]float64{1.0, 1.0, 1.0}}
 }
 
 type ToposInterface interface {
