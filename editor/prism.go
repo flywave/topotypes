a0c5d267eb9db623aa885472e8e3036edf09c718
aninggo <aninggo@gmail.com>
2022-06-06 16:36:58 +0800

    update

--- editor/prism.go
+++ editor/prism.go
@@ -9,10 +9,9 @@ import (
 
 type Prism struct {
 	BaseComponent
-	Profile      profile.Profile      `json:"profile,omitempty"`
-	UntilProfile profile.Profile      `json:"until_profile,omitempty"`
-	Direction    [3]float64           `json:"direction"`
-	Materials    []*material.Material `json:"materials,omitempty"`
+	Profile   profile.Profile      `json:"profile,omitempty"`
+	Direction [3]float64           `json:"direction"`
+	Materials []*material.Material `json:"materials,omitempty"`
 }
 
 func PrismUnMarshal(js []byte) (*Prism, error) {
