4c8c63d15cf99e28b21b3e027511aa9190dcd6c9
aninggo <aninggo@gmail.com>
2022-06-07 09:04:18 +0800

    update

--- editor/solid.go
+++ editor/solid.go
@@ -7,6 +7,7 @@ type Edge [][3]float64
 type Face []Edge
 
 type Solid struct {
+	BaseComponent
 	Faces     []Face               `json:"faces,omitempty"`
 	Materials []*material.Material `json:"materials,omitempty"`
 }
