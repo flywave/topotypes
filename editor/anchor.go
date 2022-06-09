4bc9b47fb54e315c274276f1df101503bd240441
aninggo <aninggo@gmail.com>
2022-06-09 22:39:32 +0800

    update

--- editor/anchor.go
+++ editor/anchor.go
@@ -3,9 +3,11 @@ package editor
 import vec3d "github.com/flywave/go3d/float64/vec3"
 
 type Anchor struct {
-	Center vec3d.T `json:"center"`
-	Name   string  `json:"name"`
-	NameZh string  `json:"name_zh"`
-	Normal vec3d.T `json:"normal"`
-	Unit   float64 `json:"unit"`
+	Center    vec3d.T `json:"center"`
+	Name      string  `json:"name"`
+	NameZh    string  `json:"name_zh"`
+	Normal    vec3d.T `json:"normal"`
+	Unit      float64 `json:"unit"`
+	AngleFree *bool   `json:"angle_free,omitempty"`
+	ScaleFree *bool   `json:"scale_free,omitempty"`
 }
