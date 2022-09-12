67c598d15d642abc25e804282aedf689679e7ca3
aninggo <aninggo@gmail.com>
2022-09-12 16:50:08 +0800

    u

--- decal.go
+++ decal.go
@@ -1,9 +1,7 @@
 package topotypes
 
 import (
-	dmat4 "github.com/flywave/go3d/float64/mat4"
 	dquat "github.com/flywave/go3d/float64/quaternion"
-	dvec3 "github.com/flywave/go3d/float64/vec3"
 )
 
 type TopoDecalRef struct {
@@ -12,27 +10,15 @@ type TopoDecalRef struct {
 
 type TopoProjector struct {
 	Orientation *[3]float64 `json:"orientation"`
-	Position    *[3]float64 `json:"position"`
 	Size        *[3]float64 `json:"size"`
 }
 
 func (p *TopoProjector) GetTransform() *TopoTransform {
-	posMat := dmat4.Ident
-	pos := dvec3.T(*p.Position)
-	posMat.SetTranslation(&pos)
-
-	quatMat := dmat4.Ident
 	quat := dquat.FromEulerAngles(p.Orientation[0], p.Orientation[1], p.Orientation[2])
-	quatMat.AssignQuaternion(&quat)
-
-	result := dmat4.Ident
-	result.AssignMul(&posMat, &quatMat)
-
-	t, q, _ := dmat4.Decompose(&result)
 
 	return &TopoTransform{
-		Rotation:  (*[4]float64)(q),
-		Translate: (*[3]float64)(t),
+		Rotation:  (*[4]float64)(&quat),
+		Translate: &[3]float64{0, 0, 0},
 		Scale:     &[3]float64{1.0, 1.0, 1.0},
 	}
 }
