15faad8a30763606236a9cd88505cfca3ad05a60
aninggo <aninggo@gmail.com>
2022-09-12 17:39:30 +0800

    u

--- component/component.go
+++ component/component.go
@@ -11,7 +11,7 @@ const (
 	COMPONENT_TYPE_SOLID
 	COMPONENT_TYPE_CATENARY
 	COMPONENT_TYPE_STEEL_STRUCTURE
-	COMPONENT_TYPE_BOARD
+	COMPONENT_TYPE_DECAL
 )
 
 func ComponentTypeToString(tp int) string {
@@ -30,8 +30,8 @@ func ComponentTypeToString(tp int) string {
 		return "steel-structure"
 	case COMPONENT_TYPE_CATENARY:
 		return "catenary"
-	case COMPONENT_TYPE_BOARD:
-		return "board"
+	case COMPONENT_TYPE_DECAL:
+		return "decal"
 	default:
 		return ""
 	}
@@ -52,8 +52,8 @@ func StringToComponentType(tp string) int {
 		return COMPONENT_TYPE_STEEL_STRUCTURE
 	} else if utils.StrEquals(tp, "catenary") {
 		return COMPONENT_TYPE_CATENARY
-	} else if utils.StrEquals(tp, "board") {
-		return COMPONENT_TYPE_BOARD
+	} else if utils.StrEquals(tp, "decal") {
+		return COMPONENT_TYPE_DECAL
 	}
 	return COMPONENT_TYPE_NONE
 }
--- decal.go
+++ decal.go
@@ -1,49 +1,13 @@
 package topotypes
 
-import (
-	dquat "github.com/flywave/go3d/float64/quaternion"
-)
-
 type TopoDecalRef struct {
 	Ref string `json:"ref"`
 }
 
-type TopoProjector struct {
-	Orientation *[3]float64 `json:"orientation"`
-	Size        *[2]float64 `json:"size"`
-	Depth       *float64    `json:"depth"`
-}
-
-func (p *TopoProjector) GetTransform() *TopoTransform {
-	quat := dquat.FromEulerAngles(p.Orientation[0], p.Orientation[1], p.Orientation[2])
-
-	return &TopoTransform{
-		Rotation:  (*[4]float64)(&quat),
-		Translate: &[3]float64{0, 0, 0},
-		Scale:     &[3]float64{1.0, 1.0, 1.0},
-	}
-}
-
 type TopoDecal struct {
-	Projector TopoProjector  `json:"projector"`
-	Type      string         `json:"type"`
-	Texture   string         `json:"texture"`
-	Refs      []TopoDecalRef `json:"targets,omitempty"`
-	Fusion    bool           `json:"fusion"`
-}
-
-func (tp *TopoDecal) GetTransform() *TopoTransform {
-	return tp.Projector.GetTransform()
-}
-
-func (tp *TopoDecal) GetType() string {
-	return tp.Type
-}
-
-func (tp *TopoDecal) GetFusion() bool {
-	return tp.Fusion
-}
-
-func (tp *TopoDecal) ResetTransform() {
-	tp.Projector = TopoProjector{}
+	Topos
+	Size    *[2]float64    `json:"size"`
+	Depth   *float64       `json:"depth"`
+	Texture string         `json:"texture"`
+	Refs    []TopoDecalRef `json:"targets,omitempty"`
 }
