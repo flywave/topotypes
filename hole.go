086ce59b369392e90a1d2887231c51e12a2fe531
aninggo <aninggo@gmail.com>
2022-09-15 18:03:51 +0800

    u

--- hole.go
+++ hole.go
@@ -2,8 +2,11 @@ package topotypes
 
 type TopoHole struct {
 	TopoMaker
-	Depth  *float64    `json:"depth"`
-	Fillet *[4]float64 `json:"fillet"`
+	Depth     *float64    `json:"depth,omitempty"`
+	Fillet    *[4]float64 `json:"fillet,omitempty"`
+	Offset    *[2]float64 `json:"offset,omitempty"`
+	Border    *[2]float64 `json:"border,omitempty"`
+	HasBottom *bool       `json:"has-bottom,omitempty"`
 }
 
 func NewTopoHole() *TopoHole {
